333f980ca61134b3816346c82d2a290c640a0ad3
ADWESH <adweshshaddie@gmail.com>
2019-12-14 13:41:20 +0300

    Test the constructor to see if it instantiates correctly

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -1,5 +1,11 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EncryptTest {
-
+    @Test
+    public void encryption_objectInstantiates() {
+        Encrypt enc = new Encrypt();
+        assertEquals(true, enc instanceof Encrypt);
+    }
 }
